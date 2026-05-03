d45388f87d15c39d1c9a39f073aa5e7099150360
TechBeme <TechBeme@users.noreply.github.com>
2026-05-03 00:42:39 -0300

    Remove next-intl plugin and simplify locale layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,24 +1,7 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { ThemeProvider } from "next-themes";
-import { Toaster } from "@/components/ui/sonner";
-
-export default async function LocaleLayout({
+export default function LocaleLayout({
     children,
-    params,
 }: {
     children: React.ReactNode;
-    params: Promise<{ locale: string }>;
 }) {
-    const { locale } = await params;
-    const messages = await getMessages({ locale });
-
-    return (
-        <NextIntlClientProvider messages={messages}>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-                {children}
-                <Toaster position="top-right" richColors />
-            </ThemeProvider>
-        </NextIntlClientProvider>
-    );
+    return <>{children}</>;
 }
