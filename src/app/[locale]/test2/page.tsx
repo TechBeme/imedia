e8095e9dcb6f6f9006008138e695d761afe84034
TechBeme <TechBeme@users.noreply.github.com>
2026-05-03 00:41:15 -0300

    Add test2 page in locale group

---
+++ src/app/[locale]/test2/page.tsx
@@ -0,0 +1,8 @@
+export default function Test2Page() {
+    return (
+        <div>
+            <h1>Test2 Page</h1>
+            <p>This is inside [locale] route group.</p>
+        </div>
+    );
+}
